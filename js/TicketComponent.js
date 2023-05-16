46b2ad153c331b61c0d44101933a3d18648e9eca
OEspejol <100763991+OEspejol@users.noreply.github.com>
2023-05-16 13:12:36 +0200

    yessir

--- js/TicketComponent.js
+++ js/TicketComponent.js
@@ -25,21 +25,29 @@ class TicketComponent extends HTMLElement {
         const preufinaltotal = document.createElement('p');
         preufinaltotal.classList.add('h6', 'mb-0');
         preufinaltotal.textContent = 'Preu final total:';
+        preufinaltotal.style.cssText = "font-size: 20px; color: #4CAF50; font-weight: bold; margin-bottom: 10px;";
         totalContainer.appendChild(preufinaltotal);
+        
 
         this.priceSpan = document.createElement('span');
         this.priceSpan.classList.add('price', 'h5', 'fw-bold');
         this.priceSpan.textContent = '0.00 €';
+        this.priceSpan.style.cssText = "background-color: white;color: #4CAF50; margin-bottom: 10px; background-color: #f2f2f2; border-radius: 20px;";
         totalContainer.appendChild(this.priceSpan);
+        
 
         container.appendChild(totalContainer);
 
         const clearButton = document.createElement('button');
         clearButton.classList.add('btn', 'btn-danger');
-        clearButton.textContent = 'Netejar Comanda';
+        clearButton.textContent = 'Enviar la comanda';
+        clearButton.style.cssText = "background-color: #4CAF50; color: white; border: none; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 12px; transition: background-color 0.3s ease;";
+        clearButton.onmouseover = function() { this.style.backgroundColor = "#45a049"; }
+        clearButton.onmouseout = function() { this.style.backgroundColor = "#4CAF50"; }
+        
 
         clearButton.addEventListener('click', () => {
-            const confirmation = confirm('Estàs segur que vols netejar la comanda?');
+            const confirmation = confirm('Estàs segur que vols enviar la comanda?');
             if (confirmation) {
                 this.clearOrderList();
             }
@@ -49,10 +57,14 @@ class TicketComponent extends HTMLElement {
 
         shadow.appendChild(container);
     }
-
+    // Afegir elements
     addOrder({ name, price, category }) {
         const orderItem = document.createElement('li');
         orderItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center', 'mb-2');
+        orderItem.style.cssText = "list-style-type: none; background-color: #007bff; color: white; font-size: 18px; padding: 15px 20px; border-radius: 10px; margin-bottom: 10px; transition: background-color 0.3s ease;";
+        orderItem.onmouseover = function() { this.style.backgroundColor = "#0056b3"; }
+        orderItem.onmouseout = function() { this.style.backgroundColor = "#007bff"; }
+        
 
         const nameDiv = document.createElement('div');
         nameDiv.textContent = `Nom: ${name}`;
@@ -76,6 +88,9 @@ class TicketComponent extends HTMLElement {
         const deleteButton = document.createElement('button');
         deleteButton.classList.add('btn', 'btn-sm', 'btn-danger');
         deleteButton.textContent = 'Eliminar';
+        deleteButton.style.cssText = "background-color: #ff4d4d; color: white; border: none; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 12px; transition: background-color 0.3s ease;";
+        deleteButton.onmouseover = function() { this.style.backgroundColor = "#cc0000"; }
+        deleteButton.onmouseout = function() { this.style.backgroundColor = "#ff4d4d"; }        
         orderItem.appendChild(deleteButton);
 
         this.orderList.appendChild(orderItem);
@@ -91,7 +106,7 @@ class TicketComponent extends HTMLElement {
 
         alert(`Producto seleccionado: ${name}`);
     }
-
+    //Eliminar items
     removeOrder(orderItem, price) {
         this.orderList.removeChild(orderItem);
         this.totalPrice -= price;
